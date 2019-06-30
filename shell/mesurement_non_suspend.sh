95ba8b418e33ba9296b089a86080d822e05361c4
Sekiguchi Riku <l.life.style.of.sustainability@gmail.com>
2019-06-30 20:23:51 +0900

    add parformase compare shell

---
+++ shell/mesurement_non_suspend.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# リクエスト数
+max=100
+
+# 計測開始
+start_time=`date +%s%M`
+echo $start_time
+
+# リクエスト
+for i in `seq $max`
+do
+    curl GET http://localhost:8080/non_suspend -o /dev/null -s &
+done
+
+# 終了待ち
+wait
+
+end_time=`date +%s%M`
+echo $end_time
+time=$((end_time - start_time))
+
+echo $time
