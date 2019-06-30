f503b1182b8726fdc81380dfcd563bc06d55aecf
Sekiguchi Riku <l.life.style.of.sustainability@gmail.com>
2019-06-30 20:39:21 +0900

    fix remove coroutieScope

--- src/main/kotlin/com/example/SpringBootWithCoroutine/domain/service/EmployeeService.kt
+++ src/main/kotlin/com/example/SpringBootWithCoroutine/domain/service/EmployeeService.kt
@@ -2,16 +2,15 @@ package com.example.SpringBootWithCoroutine.domain.service
 
 import com.example.SpringBootWithCoroutine.domain.entity.Employee
 import com.example.SpringBootWithCoroutine.repository.EmployeeRepository
-import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.runBlocking
 import org.springframework.stereotype.Service
 
 @Service
 class EmployeeService (private val employeeRepository: EmployeeRepository) {
 
-    suspend fun getAllEmployee(): List<Employee> = coroutineScope {
-        employeeRepository.getAllEmployee().body() ?: throw Exception()
-    }
+    suspend fun getAllEmployee(): List<Employee> =
+            employeeRepository.getAllEmployee().body() ?: throw Exception()
+
 
     fun getAllEmployeeNonSuspend(): List<Employee> = runBlocking {
         employeeRepository.getAllEmployee().body() ?: throw Exception()
